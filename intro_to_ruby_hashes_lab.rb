666d260a995d0a4364216e0ce71c98f3459b8578
James Rogers <jarogers095@gmail.com>
2019-11-28 23:58:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -49,7 +49,7 @@ def monopoly_with_fourth_tier
     railroads: {
       names: {
         reading_railroad: {
-          mortgage_value: "$ 100"
+          mortgage_value: "$100"
         },
         shortline_railroad: {
           mortgage_value: "$100"
